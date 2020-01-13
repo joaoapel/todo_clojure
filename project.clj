73458abedb0efb88b5c4aa7c80e019b03500a0ea
João Fernando Apel Miguel <joaoapel@gmail.com>
2020-01-13 04:52:15 -0300

    Biblioteca Ring adicionada

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/practicalli/clojure-todo-list-example"
   :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
             :url  "https://creativecommons.org"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns todo-list.core})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                  [ring "1.8.0"]]
+  :repl-options {:init-ns todo-list.core}
+  :main todo-list.core)
